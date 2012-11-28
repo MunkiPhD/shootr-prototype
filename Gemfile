a3afcfed8a186db8c11f9969bf3fc14db526ec89
MunkiPhD <munkiphd@gmail.com>
2012-11-27 19:24:43 -0500

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+
+gem 'mongoid'
+gem 'bson_ext'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  # gem 'therubyracer', :platforms => :ruby
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShootrPrototype::Application.config.secret_token = '4394f77e5a2475b8976cabd373ad27bdd9b61a104b682dad5c81d75eeb4d4c427dd448edcd3f18956b3ca8fccfb73acefcb8f18ac41e7c0872c8228c20961d9a'
